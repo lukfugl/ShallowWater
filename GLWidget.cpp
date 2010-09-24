efc14f6c19be45188b6c4b4f0243dd41d5d6f4a2
Jacob Fugal <lukfugl@gmail.com>
2010-09-24 10:19:49 -0600

    working

--- GLWidget.cpp
+++ GLWidget.cpp
@@ -39,7 +39,6 @@ void GLWidget::initializeGL() {
 
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
-    glEnable(GL_AUTO_NORMAL);
 }
 
 void GLWidget::resizeGL(int w, int h) {
@@ -54,6 +53,9 @@ void GLWidget::paintGL() {
     static double x[W*H];
     static double y[W*H];
     static double z[W*H];
+    static double nx[W*H];
+    static double ny[W*H];
+    static double nz[W*H];
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glPushMatrix();
@@ -62,24 +64,77 @@ void GLWidget::paintGL() {
     glRotatef(rotX, 1, 0, 0);
     glRotatef(rotY, 0, 1, 0);
 
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
     glShadeModel(GL_SMOOTH);
 
     int i, j, n;
+    double dx = 2.0 / W, dy = 2.0 / H;
     for (j = 0, n = 0; j < H; ++j) {
         for (i = 0; i < W; ++i, ++n) {
-            x[n] = i * 2.0 / W - 1;
-            y[n] = j * 2.0 / H - 1;
+            x[n] = i * dx - 1;
+            y[n] = j * dy - 1;
             z[n] = model[n].h() - 1;
+        }
+    }
+
+    double q, top, right, bottom, left, up = dx * dy;
+    for (j = 0, n = 0; j < H; ++j) {
+        for (i = 0; i < W; ++i, ++n) {
+            if (j < H-1)
+                top = dx * (z[n] - z[n+W]);
+            if (i < W-1)
+                right = dy * (z[n] - z[n+1]);
+            if (j > 0)
+                bottom = dx * (z[n-W] - z[n]);
+            if (i > 0)
+                left = dy * (z[n-1] - z[n]);
+
+            if (j < H-1) {
+                if (i > 0) {
+                    // top->left
+                    q = 1 / sqrt(left*left + top*top + up*up);
+                    nx[n] += left * q;
+                    ny[n] += top  * q;
+                    nz[n] += up   * q;
+                }
+                if (i < W-1) {
+                    // right->top
+                    q = 1 / sqrt(right*right + top*top + up*up);
+                    nx[n] += right * q;
+                    ny[n] += top   * q;
+                    nz[n] += up    * q;
+                }
+            }
+            if (j > 0) {
+                if (i < W-1) {
+                    // bottom->right
+                    q = 1 / sqrt(right*right + bottom*bottom + up*up);
+                    nx[n] += right  * q;
+                    ny[n] += bottom * q;
+                    nz[n] += up     * q;
+                }
+                if (i > 0) {
+                    // left->bottom
+                    q = 1 / sqrt(left*left + bottom*bottom + up*up);
+                    nx[n] += left   * q;
+                    ny[n] += bottom * q;
+                    nz[n] += up     * q;
+                }
+            }
+            // normalize
+            q = 1 / sqrt(nx[n]*nx[n] + ny[n]*ny[n] + nz[n]*nz[n]);
+            nx[n] *= q;
+            ny[n] *= q;
+            nz[n] *= q;
         }
     }
 
     for (j = 0, n = 0; j < H - 1; ++j) {
         glBegin(GL_TRIANGLE_STRIP);
         for (i = 0; i < W; ++i, ++n) {
-            glVertex3f(x[n], z[n], y[n]); 
-            glVertex3f(x[n+W], z[n+W], y[n+W]); 
+            glNormal3f(nx[n], nz[n], -ny[n]); 
+            glVertex3f(x[n], z[n], -y[n]); 
+            glNormal3f(nx[n+W], nz[n+W], -ny[n+W]); 
+            glVertex3f(x[n+W], z[n+W], -y[n+W]); 
         }
         glEnd();
     }
